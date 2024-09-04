cb4ea07a3c9326b852315dc4ff116bb6e3c4d8b3
Ramses <rambandan@gmail.com>
2024-09-03 22:56:28 -0500

    Initial commit

---
+++ src/AppLaucher.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+public class AppLaucher {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new WeatherAppGui().setVisible(true);
+            }
+        });
+    }
+}
